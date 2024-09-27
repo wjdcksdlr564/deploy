02f5cde227a30e55930767a794c71e6c410d7e9e
wjdcksdlr564 <wjdcksdlr564@naver.com>
2024-09-27 11:39:36 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM amazoncorretto:11-alpine-jdk
+ARG JAR_FILE=target/*.jar
+ARG PROFILES
+ARG ENV
+COPY ${JAR_FILE} app.jar
+
+ENTRYPOINT ["java", "-Dspring.profiles.active=blue", "-jar", "app.jar"]
---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${client.address}")
+    private String clientAddress;
+
+    @CrossOrigin
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok(clientAddress + "테스트!!");
+    }
+}
