88d546ce5f3e378c4fa4cf8c746896cb2f6ddcdd
wjdcksdlr564 <wjdcksdlr564@naver.com>
2024-09-30 17:22:19 +0900

    이름 추가

--- src/main/java/com/study/deploy_back/controller/TestController.java
+++ src/main/java/com/study/deploy_back/controller/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<?> test() {
-        return ResponseEntity.ok(clientAddress + "테스트!!");
+        return ResponseEntity.ok(clientAddress + "테스트!! 정찬익");
     }
 }
